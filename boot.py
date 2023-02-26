520b25fcaafc072ef189e36d84cb5af109c74dee
TURFPTAx <40579046+turfptax@users.noreply.github.com>
2023-02-25 23:04:34 -0600

    udodec main
    main code

--- boot.py
+++ boot.py
@@ -1,32 +1,1 @@
-
-
-import udodec
-
-print('import complete')
-
-#udodec.run_once()
-udodec.run_once()
-
-face = [-1,-9,-7,-5,-3]
-trios = [1,-1,30,-30,24,-24,17,-17,13,-13,6,-6]
-quartets = [-10,11,12,-15,16,17,-5,6,7,-22,-26,-30]
-
-for i in quartets:
-  r = udodec.random.randint(0,32)
-  g = udodec.random.randint(0,32)
-  b = udodec.random.randint(0,32)
-  udodec.pixel.append([i,2,1,1,'RL',(r,g,b),60])
-  udodec.pixel.append([i,1,1,1,'RL',(r,g,b),60])
-  udodec.pixel.append([i,0,1,1,'RL',(r,g,b),60])
-
-
-
-for i in range(1000):
-  udodec.move()
-  udodec.np.write()
-  #udodec.dim()
- 
-
-
-
-
+import udodecprint('import complete')#udodec.run_once()udodec.run_once()face = [-1,-9,-7,-5,-3]trios = [1,-1,30,-30,24,-24,17,-17,13,-13,6,-6]quartets = [-10,11,12,-15,16,17,-5,6,7,-22,-26,-30]udodec.draw_quartet(100)print('Fin')udodec.pixel = [] for i in range(12):    print(f'face:{i}')    udodec.draw_face(i,55)print('fin 2')
--- udodec.py
+++ udodec.py
@@ -1,199 +1,1 @@
-
-
-
-#udodec.py
-
-
-import neopixel
-import machine
-import time
-import gc
-import random
-print('hello world!')
-
-np = neopixel.NeoPixel(machine.Pin(39),300)
-
-verts= [[-1,2,3],
-[-3,4,5],
-[-5,6,7],
-[-7,8,9],
-[-9,10,1],
-[-4,-17,18],
-[-14,15,-2],
-[-10,11,12],
-[-8,-23,24],
-[-6,-20,21],
-[20,-18,19],
-[17,-15,16],
-[14,-12,13],
-[-11,-24,25],
-[23,-21,22],
-[-19,-29,30],
-[29,-16,-28],
-[28,-13,-27],
-[27,-25,26],
-[-26,-22,-30]] 
-sequence = 'RLLRLLLLBLL'
-die_rate = 20
-pixel = []
-
-for i in range(30):
-  r = random.randint(0,127)
-  g = random.randint(0,127)
-  b = random.randint(0,127)
-  #pixel.append([i+1,0,1,1,sequence,(r,g,b),60])
-
-edges = []
-for i in range(31):
-  edges.append([i,0])
-
-
-def run_once(np=np):
-  for i in range(300):
-    np[i] = (255,127,0)
-    np.write()
-    time.sleep(.01)
-    np[i] = (0,0,0)
-    np.write()
-  return
-  
-  
-def count_edges(np=np):
-  for i in range(30):
-    for a in range(10):
-      np[(i*10)+a] = (127,127,255)
-    np.write()
-    time.sleep(1)
-    print(f'group:{i*10}-{((i+1)*10)-1}')
-    input()
-    for a in range(10):
-      np[(i*10)+a] = (27,0,0)
-    np.write()
-    
-    
-    
-def convert_directions(text):
-  numeric = []
-  for i in text:
-    if i == 'R':
-      numeric.append(-2)
-    if i == 'L':
-      numeric.append(-1)
-    if i == 'B':
-      numeric.append(-3)
-  return numeric
-  
-def change_color(cp,rgb):
-  global edges
-  hits = edges[abs(cp)][1]
-  new_rgb = [0,0,0]
-
-  new_rgb[hits%3] += 11
-  r = rgb[0] + new_rgb[0]
-  g = rgb[1] + new_rgb[1]
-  b = rgb[2] + new_rgb[2]
-  edges[abs(cp)][1] += 1
-  return (r,g,b)
-  
-  
-  
-# have to give it directions:pattern and how many times:runs
-def iterate_route(pattern,runs,verts=verts,np=np):
-  cp = -1
-  pat = convert_directions(pattern)
-  print(f'Pattern Numeric Array:{pat}')
-  for r in range(runs):
-    for p in pat:
-      if cp > 0:
-        color = change_color(cp,np[(cp*10)-10])
-      else:
-        color = change_color(cp,np[(-cp*10)-10])
-      for y in range(10):
-        if cp > 0:
-          np[(cp*10)-10+y] = color
-        elif cp < 0:
-          np[(-cp*10)-10+(9-y)] = color
-        np.write()
-        time.sleep(.1)
-        np.write()
-        #time.sleep(.01)
-      #Pattern code
-      found = False
-      for i,x in enumerate(verts):
-        if -cp in x and not found:
-          print(f'cp:{cp}',f'Vertici:{x}',f'pattern:{p}')
-          indie = x.index(-cp)
-          cp = x[indie+p]
-          print(f'new cp:{cp} indie:{indie} p:{p}')
-          found = True
- 
-
-      
-def dim(die_rate=die_rate,np=np):
-  for i in range(len(np)):
-    if np[i][0] + np[i][1] + np[i][2] > 0:
-      r = np[i][0] - die_rate
-      g = np[i][1] - die_rate
-      b = np[i][2] - die_rate
-      if r < 0:
-        r = 0
-      if g < 0:
-        g = 0
-      if b < 0:
-        b = 0
-      np[i] = (r,g,b)
-  #np.write()
-    
-def move(verts=verts,np=np):
-  global pixel
-  newp = None
-  gc.collect()
-  newp = []
-  for p in pixel:
-    edge = p[0]
-    pix = p[1]
-    l = p[2]
-    speed = p[3]
-    pattern = p[4]
-    adjustment = p[5]
-    ttl = p[6]
-    pat = convert_directions(pattern)
-    found = False
-    for num in range(speed):
-        if pix + 1 > 9:
-          ttl -= 1
-          if edge > 0:
-            np[(edge*10)-10+pix] = (0,0,0)
-          elif edge < 0:
-            np[(-edge*10)-10+(9-pix)] = (0,0,0)
-          for i,x in enumerate(verts):
-            if -edge in x and not found:
-              #print(f'edge:{edge}',f'Vertici:{x}',f'pattern:{p}')
-              indie = x.index(-edge)
-              edge = x[indie+pat[0]]
-              #print(f'new edge:{edge} indie:{indie} pat[0]:{pat[0]}')
-              found = True
-          pattern = pattern[1:] + pattern[0]
-          pix = pix + 1 - 10
-        else:
-          if edge > 0:
-            np[(edge*10)-10+pix] = (0,0,0)
-          elif edge < 0:
-            np[(-edge*10)-10+(9-pix)] = (0,0,0)
-          pix = pix + 1
-        if edge > 0:
-          np[(edge*10)-10+pix] = adjustment
-        elif edge < 0:
-          np[(-edge*10)-10+(9-pix)] = adjustment
-    #np.write()
-    if ttl:
-      newp.append([edge,pix,l,speed,pattern,adjustment,ttl])
-  pixel = newp
-
-
-print('right before!')
-#iterate_route('RLLRLLLLBLL',50)
-
-
-
-
+#udodec.pyimport neopixelimport machineimport timeimport gcimport randomprint('udodec.py micropython dodecahedron module')catch = 'not a number'np = neopixel.NeoPixel(machine.Pin(39),300)# Vertices should be named by edge count direction# Negative for moving toward verticy + for away# Must be in counter clockwise order for translationverts= [[-1,2,3],[-3,4,5],[-5,6,7],[-7,8,9],[-9,10,1],[-4,-17,18],[-14,15,-2],[-10,11,12],[-8,-23,24],[-6,-20,21],[20,-18,19],[17,-15,16],[14,-12,13],[-11,-24,25],[23,-21,22],[-19,-29,30],[29,-16,-28],[28,-13,-27],[27,-25,26],[-26,-22,-30]] faces = [[1,3,5,7,-9],[9,8,24,-11,-10],[-8,-7,6,21,23],[6,-20,-18,4,5],[-4,-3,2,15,17],[-2,-1,10,12,14],[-12,11,25,27,-13],[13,28,-16,-15,-14],[16,29,-19,-18,-17],[19,30,-22,-21,-20],[22,-26,-25,-24,-23],[26,-30,-29,-28,-27]]# My favorite dodec sequence -TURFPTAx RLLRLLLLBLLsequence = 'RLLRLLLLBLL'tracers = Falsedie_rate = 20pixel = []def run_once(np=np):    for i in range(300):        np[i] = (127,64,0)    np.write()    np.buf = bytearray(900)    np.write()    for i in range(300):        np[i] = (255,127,0)    np.write()    np.buf = bytearray(900)    np.write()    for i in range(300):        np[i] = (255,255,0)    np.write()    np.buf = bytearray(900)    np.write()    return    # Help to count the edges of dodecahedrondef count_edges(np=np):    for i in range(30):        for a in range(10):            np[(i*10)+a] = (127,127,255)        np.write()        time.sleep(1)        print(f'group:{i*10}-{((i+1)*10)-1}')        input()        for a in range(10):            np[(i*10)+a] = (27,0,0)        np.write()        def draw_trios(duration,speed=3,clear=True):    global pixel    if clear:        pixel = []    trios = [1,-1,30,-30,24,-24,17,-17,13,-13,6,-6]    for i in trios:        r = random.randint(0,127)        g = random.randint(0,127)        b = random.randint(0,127)        pixel.append([i,0,1,3,'RLLRLLLLBLL',(r,g,b),55])        pixel.append([i,0,1,1,'RLLRLLLLBLL',(r,g,b),55])    for i in range(duration):        move() def draw_quartet(duration,clear=True):    global pixel    if clear:        pixel = []    quartets = [-10,11,12,-15,16,17,-5,6,7,-22,-26,-30]    for i in quartets:        r = random.randint(0,127)        g = random.randint(0,127)        b = random.randint(0,127)        pixel.append([i,0,1,3,'RLLRLLLLBLL',(r,g,b),55])        pixel.append([i,0,1,1,'RLLRLLLLBLL',(r,g,b),55])    for i in range(duration):        move()        def draw_face(face,duration,clear=True):    global pixel    global faces    if clear:        pixel = []    for i in faces[face]:        r = random.randint(0,127)        g = random.randint(0,127)        b = random.randint(0,127)        pixel.append([i,0,1,3,'RLLRLLLLBLL',(r,g,b),55])        pixel.append([i,0,1,1,'RLLRLLLLBLL',(r,g,b),55])    for i in range(duration):        move()    def convert_directions(text):    numeric = []    for i in text:        if i == 'R':            numeric.append(-2)        if i == 'L':            numeric.append(-1)        if i == 'B':            numeric.append(-3)    return numeric    def change_color(cp,rgb):    global edges    hits = edges[abs(cp)][1]    new_rgb = [0,0,0]    new_rgb[hits%3] += 11    r = rgb[0] + new_rgb[0]    g = rgb[1] + new_rgb[1]    b = rgb[2] + new_rgb[2]    edges[abs(cp)][1] += 1    return (r,g,b)    def dim(die_rate=die_rate,np=np):    for i in range(len(np)):        if np[i][0] + np[i][1] + np[i][2] > 0:            r = np[i][0] - die_rate            g = np[i][1] - die_rate            b = np[i][2] - die_rate            if r < 0:                r = 0            if g < 0:                g = 0            if b < 0:                b = 0            np[i] = (r,g,b)def move(verts=verts,np=np):    global tracers    global pixel    global catch    newp = None    gc.collect()    newp = []    if tracers:            dim()    else:        np.buf = bytearray(900)    for p in pixel:        edge = p[0]        pix = p[1]        l = p[2]        speed = p[3]        pattern = p[4]        adjustment = p[5]        ttl = p[6]        pat = convert_directions(pattern)        found = False        catch = pix        for num in range(speed):            if pix + 1 > 9:                ttl -= 1                for i,x in enumerate(verts):                    if -edge in x and not found:                        #print(f'edge:{edge}',f'Vertici:{x}',f'pattern:{p}')                        indie = x.index(-edge)                        edge = x[indie+pat[0]]                        #print(f'new edge:{edge} indie:{indie} pat[0]:{pat[0]}')                        found = True                pattern = pattern[1:] + pattern[0]                pix = pix + 1 - 10            else:                pix = pix + 1            if edge > 0:                np[(edge*10)-10+pix] = adjustment            elif edge < 0:                np[(-edge*10)-10+(9-pix)] = adjustment        if ttl < 0:            ttl = -1        if ttl != 0:            newp.append([edge,pix,l,speed,pattern,adjustment,ttl])    np.write()    pixel = newpprint('last line dodec.py')
